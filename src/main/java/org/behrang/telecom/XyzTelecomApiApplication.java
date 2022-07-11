9d36fee66ec7e91711bd782f6fe011c34337895a
Behrang Saeedzadeh <hello@behrang.org>
2022-07-11 17:29:07 +1000

    wip

---
+++ src/main/java/org/behrang/telecom/XyzTelecomApiApplication.java
@@ -0,0 +1,13 @@
+package org.behrang.telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzTelecomApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyzTelecomApiApplication.class, args);
+    }
+
+}
